5f187261b24f180b1b635f14383078c6ba1d8f52
vanderson1022 <vanderson1022@gmail.com>
2025-05-18 15:17:07 -0300

    simulando a correção do bug e testes multi-browser

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -12,7 +12,7 @@ test('senha obrigatória', async ({ page }) => {
 
 test('usuário não existe', async ({ page }) => {
   await login(page,'teste', 'teste')
-  await toast(page, 'Oops! senha incorreta :(')
+  await toast(page, 'Oops! Credenciais inválidas :(')
 })
 
 test('senha incorreta', async ({ page }) => {
